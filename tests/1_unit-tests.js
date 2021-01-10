39ce6789c1d7eead0fedda38d901b93bf68925f5
achyut635 <achyut635@gmail.com>
2021-01-09 19:39:00 -0500

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const assert = chai.assert;
 
 
-  suite('Basic Assertions', function () {
+  
     // #1
     test('#isNull, #isNotNull', function () {
       assert.isNull(null, 'this is an optional error description - e.g. null is null');
@@ -10,4 +10,4 @@ const assert = chai.assert;
     });
     
   // -----------------------------------------------------------------------------
-});
+
